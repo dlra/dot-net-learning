264d353405ae8ee2fab0f1f71ecdb7dc9d3a9985
daniel_ansell_geeksltd <daniel.ansell@geeks.ltd.uk>
2020-04-30 12:23:39 +0100

    Initial commit

---
+++ Test/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            IEnumerable<string> obj = new MyEnumerable();
+            
+            foreach (var item in obj)
+                Console.WriteLine(item);
+
+            foreach (var item in obj)
+                Console.WriteLine("2: " + item);
+
+            Console.ReadKey();
+        }
+        class MyEnumerable : IEnumerable<string>, IEnumerator<string>
+        {
+            readonly string[] fruit = { "Apples", "Pears", "Rhubarb" };
+            int index = 0;
+
+            string _current;
+            string IEnumerator<string>.Current => _current;
+
+            object IEnumerator.Current => _current;
+
+            void IDisposable.Dispose()
+            {
+                index = 0;
+            }
+
+            IEnumerator<string> IEnumerable<string>.GetEnumerator()
+            {
+                return this;
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return this;
+            }
+
+            bool IEnumerator.MoveNext()
+            {
+                if (index < fruit.Length)
+                    _current = fruit[index];
+
+                if (index++ == fruit.Length)
+                    return false;
+                else
+                    return true;
+            }
+
+            void IEnumerator.Reset()
+            {
+                index = 0;
+            }
+        }
+    }
+}
---
+++ Test2/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Test2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            IEnumerable<string> obj = new MyEnumerable();
+
+            foreach (var item in obj)
+                Console.WriteLine(item);
+
+            foreach (var item in obj)
+                Console.WriteLine("2: " + item);
+
+            Console.ReadKey();
+        }
+
+        class MyEnumerable : IEnumerable<string>
+        {
+            readonly string[] fruit = { "Apples", "Pears", "Rhubarb" };
+
+            Enumerator GetEnumerator()
+            {
+                return new Enumerator(this);
+            }
+
+            IEnumerator<string> IEnumerable<string>.GetEnumerator()
+            {
+                return GetEnumerator();
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return GetEnumerator();
+            }
+
+            struct Enumerator : IEnumerator<string>
+            {
+                int index;
+                MyEnumerable myEnumerable;
+
+                internal Enumerator(MyEnumerable myEnumerable)
+                {
+                    this.myEnumerable = myEnumerable;
+                    index = 0;
+                    Current = myEnumerable.fruit[index];
+                }
+
+                public string Current { get; private set; }
+
+                object IEnumerator.Current => Current;
+
+                public void Dispose()
+                {
+
+                }
+
+                public bool MoveNext()
+                {
+                    if (index < myEnumerable.fruit.Length)
+                    {
+                        Current = myEnumerable.fruit[index];
+                        index++;
+                        return true;
+                    }
+
+                    return false;
+                }
+
+                public void Reset()
+                {
+                    index = 0;
+                }
+            }
+        }
+    }
+}
